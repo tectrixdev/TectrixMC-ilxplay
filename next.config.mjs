41c920c9e4e1a557fb3c8a4a9064a6cd86bb586e
ilxplay <illiasuper46@gmail.com>
2025-02-08 22:59:50 +0100

    fix
    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
   },
+  output: 'export',  // Enables static exports
+  basePath: '/your-repo-name', // Replace with your repository name
+  images: {
+    unoptimized: true,
+  },
 
 };
 
+export default nextConfig;
 
