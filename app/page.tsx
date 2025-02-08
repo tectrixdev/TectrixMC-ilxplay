27750f961afbe8fd2ac1f4e470f946df8d6dd210
ilxplay <illiasuper46@gmail.com>
2025-02-08 22:16:43 +0100

    init

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { navItems } from "@/data";
+import { Helmet } from 'react-helmet';
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Players from "@/components/Players";
+import Approach from "@/components/Approach";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Features from "@/components/Features";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <Helmet>
+        <link rel="icon" href="/faviconTectrix.ico" />
+      </Helmet>
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        {/*<Players />*/}
+        <Features />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ data/index.ts
@@ -0,0 +1,142 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Features", link: "#features" },
+  { name: "Join Us", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Have fun together!",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end ",
+    img: "/b1.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Play with our worldwide community!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "A Comprehensive wiki",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    titleClassName: "justify-start text-center md:max-w-full max-w-60",
+    img: "wiki.jpg",
+    imgClassName: "w-full h-full",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "24/7 admin support",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Explore multiple worlds!",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-center md:justify-start lg:justify-center decoration-dashed",
+    img: "b5.png"
+  },
+  {
+    id: 6,
+    title: "Join right now!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+
+export const testimonials = [
+  {
+    quote:
+      "quote",
+    name: "player",
+    title: "...",
+  },
+  {
+    quote:
+      "",
+    name: "",
+    title: "...",
+  },
+  {
+    quote:
+      "",
+    name: "",
+    title: "...",
+  },
+  {
+    quote:
+      "",
+    name: "",
+    title: "...",
+  },
+  {
+    quote:
+      "",
+    name: "",
+    title: "...",
+  },
+];
+
+
+export const features = [
+  {
+    id: 1,
+    title: "Community driven economy system",
+    desc: "A unique, self-made economy system that makes Tectrix deeply immersive",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.png",
+  },
+  {
+    id: 2,
+    title: "In-Game Interactions system",
+    desc: "System that allows players to interact with eachother in a variety of ways from making small casual groups to forming giant business corporations",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.png",
+  },
+  {
+    id: 3,
+    title: "In-Game voice chat",
+    desc: "Communicate, interact, cooperate and have fun with everyone!",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.png",
+  },
+  {
+    id: 4,
+    title: "World Progression system",
+    desc: "Complete achievements and cooperate with other players to progress and compete in a race against time!",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.png",
+  },
+];
+
+/*export const socialMedia = [
+  {
+    id: 1,
+    img: "git.svg",
+
+  },
+];
+*/
