a45d02bd8335bd358b5cde5aeaceb8af77112166
TecTrix <joranhennion@duck.com>
2025-03-10 21:46:08 +0100

    You can't put a head in a body lil bro :skull:

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,6 @@ import Features from "@/components/Features";
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <Head>
-        <link
-          rel="icon"
-          href="faviconTectrix.ico"
-        />
-      </Head>
       <div className="max-w-7xl w-full">
         {/*<FloatingNav navItems={navItems} /> */}{" "}
         {/* potential fix for vercel */}
